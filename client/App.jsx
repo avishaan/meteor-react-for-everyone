ccc95f9416ccd52e81ff23100808b7eae1deef99
Avishaan <avishaan@codeHatcher.com>
2016-07-18 21:08:33 -0700

    add routes and react-mounter for layout, lines up with episode on page layouts

--- client/App.jsx
+++ client/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom'
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <h1>Hello World</h1>
     );
   }
-}
-
-if (Meteor.isClient) {
-  Meteor.startup(function(){
-    render(<App />, document.getElementById('render-target'));
-  });
 }
