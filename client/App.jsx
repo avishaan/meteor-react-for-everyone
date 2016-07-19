d72d68886121dfd7acd35555ca99b2aaf4104441
Avishaan <avishaan@codeHatcher.com>
2016-07-19 15:55:35 -0700

    finish step #4, insert into database using ref callback and libs/collections.js

--- client/App.jsx
+++ client/App.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 import { render } from 'react-dom'
 
 export default class App extends React.Component {
+  addResolution(event) {
+    event.preventDefault();
+    var text = this._resolution.value.trim();
+    console.log(text);
+
+    Resolutions.insert({
+      'text': text,
+      'complete': false,
+      'createdAt': new Date()
+    });
+
+    // reset string
+    this._resolution.value = "";
+  }
+
   render() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <h1>My Resolutions</h1>
+        <form className='new-resolution' onSubmit={this.addResolution.bind(this)}>
+          <input type='text' ref={(ref) => this._resolution = ref} placeholder='finish series' />
+        </form>
+      </div>
     );
   }
 }
