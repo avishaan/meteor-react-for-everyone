6f8be0c03ba84b440803f5ed3abc9fc033b25645
Avishaan <avishaan@codeHatcher.com>
2016-07-19 17:44:30 -0700

    finish step #5, but use react-meteor-data instead of TrackerReact

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom'
+import { createContainer } from 'meteor/react-meteor-data';
 
 export default class App extends React.Component {
   addResolution(event) {
@@ -18,13 +19,27 @@ export default class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <h1>My Resolutions</h1>
-        <form className='new-resolution' onSubmit={this.addResolution.bind(this)}>
-          <input type='text' ref={(ref) => this._resolution = ref} placeholder='finish series' />
-        </form>
-      </div>
-    );
+    let res = this.props.resolutions;
+    if (res.length < 1) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div>
+          <h1>My Resolutions</h1>
+          <form className='new-resolution' onSubmit={this.addResolution.bind(this)}>
+            <input type='text' ref={(ref) => this._resolution = ref} placeholder='finish series' />
+          </form>
+          <div>
+            {res[0].text}
+            </div>
+          </div>
+      );
+    }
   }
 }
+
+export default createContainer(() => {
+  return {
+    'resolutions': Resolutions.find().fetch()
+  };
+}, App);
